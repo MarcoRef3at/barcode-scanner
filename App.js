6392b03a2232521646bd2a65f5c0160974193cb5
marcoref3at <marco_refaat@hotmail.com>
2021-06-02 20:22:38 +0200

    First Commit

--- App.js
+++ App.js
@@ -1,12 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  Alert,
+  Modal,
+  TouchableHighlight,
+  TextInput,
+} from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { Ionicons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [api, setApi] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+      const recordedApi = await AsyncStorage.getItem("api");
+      if (recordedApi) {
+        setApi(recordedApi);
+      }
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    Alert.alert(
+      "Code Detected",
+      `${data}`, // <- this part is optional, you can pass an empty string
+      [
+        {
+          text: "Cancel",
+          onPress: () => {
+            setScanned(false);
+            console.log("Cancel Pressed");
+          },
+        },
+        {
+          text: "OK",
+          onPress: () => {
+            setScanned(false);
+            var request = new XMLHttpRequest();
+            request.onreadystatechange = (e) => {
+              if (request.readyState !== 4) {
+                return;
+              }
+
+              if (request.status === 200) {
+                console.log("success", request.responseText);
+              } else {
+                console.warn("error");
+              }
+            };
+
+            request.open("POST", api);
+            request.send();
+          },
+        },
+      ],
+      { cancelable: false }
+    );
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.modalView}>
+          <Text style={styles.modalText}>Enter the API</Text>
+          <TextInput
+            style={{ width: 200, borderColor: "gray", borderWidth: 1 }}
+            onChangeText={(text) => setApi(text)}
+            value={api}
+          />
+          <TouchableHighlight
+            style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+            onPress={() => {
+              setModalVisible(!modalVisible);
+              AsyncStorage.setItem("api", api);
+            }}
+          >
+            <Text style={styles.textStyle}>Save</Text>
+          </TouchableHighlight>
+        </View>
+      </Modal>
+
+      <TouchableHighlight
+        style={styles.openButton}
+        onPress={() => {
+          setModalVisible(true);
+        }}
+      >
+        <Ionicons name="settings" size={24} color="black" />
+      </TouchableHighlight>
     </View>
   );
 }
@@ -14,8 +123,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  openButton: {
+    backgroundColor: "#ffffff",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+    alignSelf: "flex-end",
+    marginTop: -5,
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
